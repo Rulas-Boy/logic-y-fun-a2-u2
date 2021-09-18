78d34d155ff4dc1c5d61feadd73adc5076f418fe
RulasBoy <raulito_a_e@hotmail.com>
2021-09-17 21:26:55 -0700

    Agrego el archivo con el prorgama terminado

---
+++ actividad2.hs
@@ -0,0 +1,13 @@
+module Actividad2 where
+
+suma :: Int -> Int -> Int
+suma x y = x + y
+
+resta :: Int -> Int -> Int
+resta x y = x - y
+
+multiplicacion :: Int -> Int -> Int
+multiplicacion x y = x * y
+
+division :: Int -> Int -> Int
+division x y = x `div` y
